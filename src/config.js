9266792add655635f5e5a6e10d0c51a454213c4f
fmakareev <fmakareev@code-artel.com>
2019-08-26 16:57:37 +0300

    add endpoints and recipeList

--- src/config.js
+++ src/config.js
@@ -3,14 +3,32 @@ export const Config = {
     ios: "https://apps.apple.com/us/app/%D1%80%D0%B5%D1%86%D0%B5%D0%BF%D1%82%D1%8B-%D0%B5%D0%B4%D0%B0/id1457887085?l=ru&ls=1",
     android: "https://play.google.com/store/apps/details?id=ru.harbordigital.edatvapp",
   },
-  socialLinks:{
-    facebook: 'https://www.facebook.com/tveda.vidorecepty',
-    twitter: 'https://twitter.com/telekanaleda',
-    instagram: 'https://www.instagram.com/edatelevision/',
-    youtube: 'https://www.youtube.com/user/EdaHDTelevision',
-    linkedin: '',
-  }
+  socialLinks: {
+    facebook: "https://www.facebook.com/tveda.vidorecepty",
+    twitter: "https://twitter.com/telekanaleda",
+    instagram: "https://www.instagram.com/edatelevision/",
+    youtube: "https://www.youtube.com/user/EdaHDTelevision",
+    linkedin: "",
+  },
+  endpoints: {
+    getRecipe: "http://api2.tveda.ru/a1/?action=get_recipe&id=",
+  },
+
+  recipeList: [
+    944669,
+    944670,
+    944671,
+    944672,
+    944673,
+    944674,
+    944675,
+    944676,
+    944677,
+    944678,
+  ],
 
 }
+
+
 
 export default Config
