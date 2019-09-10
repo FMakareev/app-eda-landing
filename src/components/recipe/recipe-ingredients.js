be5e26690902ffdfd8e2b20d47b6408d69060d01
fmakareev <fmakareev@code-artel.com>
2019-09-10 11:45:51 +0300

    Добавил склонения в еще

--- src/components/recipe/recipe-ingredients.js
+++ src/components/recipe/recipe-ingredients.js
@@ -59,13 +59,24 @@ const sliceIngredients = (ingredients, isSlice) => {
 }
 
 
+const GetMoreTitle = (_count) => {
+  let count = _count > 9 && _count !== 11 ? parseInt(_count.toString()[_count.toString().length - 1]) : _count
+  if (count > 1 && count < 5) {
+    return "ингридиента"
+  } else if (count > 4 || count === 0 || count === 11) {
+    return "ингридиентов"
+  } else if (count === 1) {
+    return "ингридиент"
+  }
+}
+
 export const RecipeIngredients = ({ ingredients, recipe }) => {
 
   const [isOpen, toggleList] = React.useState(false)
 
-  useEffect(()=>{
-    toggleList(false);
-  },[recipe]);
+  useEffect(() => {
+    toggleList(false)
+  }, [recipe])
 
   return (
     <div className="recipe_ingredients f-grid-cell">
@@ -96,7 +107,8 @@ export const RecipeIngredients = ({ ingredients, recipe }) => {
               target={"_blank"}
               href=""
             >
-              И еще {Object.entries(ingredients).slice(9).length} ингредиентов
+              И
+              еще {Object.entries(ingredients).slice(9).length} {GetMoreTitle(Object.entries(ingredients).slice(9).length)}
             </a>
           </li>
         }
